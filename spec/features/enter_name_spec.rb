868e39d553879a306d92e2cd7780ca097c464def
jesslns <jesslns@yahoo.com.hk>
2019-01-23 16:45:32 +0000

    Player 1 can attack player 2

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,20 +1,21 @@
 feature 'Enter name' do
   scenario 'Players fill in their names in a form' do
-    visit('/')
-    fill_in :player1, with: 'Jess'
-    fill_in :player2, with: 'James'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Jess vs James'
   end
 end
 
 feature 'Hit points' do
   scenario "Show Player 2 hit points" do
-    visit('/')
-    fill_in :player1, with: 'Jess'
-    fill_in :player2, with: 'James'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'James: 1 point(s)'
   end
+end
 
+feature 'Attack' do
+  scenario "Player 1 attacks Player 2" do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content 'Jess attacked James!'
+  end
 end
