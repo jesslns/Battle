d1edff47e3dcf7fb2753562638bb35f64555b257
jesslns <jesslns@yahoo.com.hk>
2019-01-23 16:19:08 +0000

    Show Player 2's hit point

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -6,5 +6,15 @@ feature 'Enter name' do
     click_button 'Submit'
     expect(page).to have_content 'Jess vs James'
   end
+end
 
+feature 'Hit points' do
+  scenario "Show Player 2 hit points" do
+    visit('/')
+    fill_in :player1, with: 'Jess'
+    fill_in :player2, with: 'James'
+    click_button 'Submit'
+    expect(page).to have_content 'James: 1 point(s)'
   end
+
+end
