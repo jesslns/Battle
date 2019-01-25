bfcc246e50cfbe69ff93f47d3e3d81a9ec124921
jesslns <jesslns@yahoo.com.hk>
2019-01-25 16:39:21 +0000

    Refactor controller, make  the global variable

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,12 @@ require_relative './player'
 
 class Game
 
-attr_reader :get_hit
+attr_reader :get_hit, :player1, :player2
+
+def initialize (player1, player2)
+  @player1 = player1
+  @player2 = player2
+end
 
 def attack(player)
   player.get_hit
